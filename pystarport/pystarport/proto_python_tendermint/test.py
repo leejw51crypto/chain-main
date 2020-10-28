8ef7ebce876908e0f4c47aa349f3d9373bbe35ac
jongwhan lee <jonghwan@crypto.com>
2020-10-28 16:21:39 +0800

    add gogo proto

--- pystarport/pystarport/proto_python_tendermint/test.py
+++ pystarport/pystarport/proto_python_tendermint/test.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-print("grpc")
+import tendermint.rpc.grpc.types_pb2_grpc
+import grpc
+
+channel= grpc.insecure_channel('localhost:9091')
+stub= tendermint.rpc.grpc.types_pb2_grpc.BroadcastAPIStub(channel)
+request = tendermint.rpc.grpc.types_pb2.RequestBroadcastTx()
+print(f'request={request}')
+response = stub.BroadcastTx(request)
+print(f'response={response}')
