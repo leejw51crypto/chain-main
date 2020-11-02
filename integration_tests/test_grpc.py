05efaf26e1a9e75cf3472e945a8636493e190ce4
jongwhan lee <jonghwan@crypto.com>
2020-11-02 14:56:35 +0800

    remove debug code

--- integration_tests/test_grpc.py
+++ integration_tests/test_grpc.py
@@ -9,35 +9,18 @@ import cosmos.staking.v1beta1.query_pb2_grpc
 import cosmos.bank.v1beta1.tx_pb2
 import cosmos.bank.v1beta1.tx_pb2_grpc
 import grpc
-import json
 
 def test_query_validators(cluster):    
-    print("go!@@@@@@@@@@@@@@@@@@@@@@@@@")
     wait_for_new_blocks(cluster,5)
-    baseport = cluster.base_port(0)
-    print(f'baseport={baseport}')
-    ipport = cluster.ipport_grpc(0)
-    print(f'grpc port={ipport}')
-    channel= grpc.insecure_channel(ipport)
+    grpc_ip_port = cluster.ipport_grpc(0)
+    channel= grpc.insecure_channel(grpc_ip_port)
     stub= cosmos.staking.v1beta1.query_pb2_grpc.QueryStub(channel)
     response=stub.Validators( cosmos.staking.v1beta1.query_pb2.QueryValidatorsRequest())
-    print(f'response={response}')
-    #f= json.dumps(response, indent=4)
-    #print(f'response={f}')
-    #o= response.operator_address
     validators= cluster.validators() 
     operators = {}
-    print(f'@@@ validators={validators}')
     for a in validators:
-        print(f'@@@@@@    {a}')
-        c=a["operator_address"]
-        print(f'c={c}')
-        operators[c] = a     
+        operator_address=a["operator_address"]
+        operators[operator_address] = a     
     
     for v in response.validators :
-        print(f"v.operator_address = {v.operator_address in operators}")
         assert v.operator_address in operators
-    #print(f'validator cli={v}')
-    print("******************************************")
-    print(f'total operators {len(operators)	}')
-    assert False
