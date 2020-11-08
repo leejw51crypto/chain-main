9ff05887dc483f2ef7513d098ac7ddae54555dc8
jongwhan <jonghwan@crypto.com>
2020-11-08 22:54:11 +0800

    Problem: missing grpc integration test (fix #194)
    Solution: add grpc query, tx broadcast integration test
    generate proto+grpc code on the fly

---
+++ pystarport/pystarport/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import ../../nix { }, commit ? "" }:
+with pkgs;
+pkgs.mkShell {                                                                                             
+buildInputs = [                                                                                     
+        python38                                                                                      
+        protobuf3_13                                                                                  
+        grpc                                                                                          
+        git                                                                                           
+];                                                                                                  
+shellHook= ''                                                                                       
+  ./convert.sh                                                                                          '';                                                                                                 
+}
+
